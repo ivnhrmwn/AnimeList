cd45b93aba9736c303f219f21c6f0158385c04c0
vann711 <ivanhermawan1253@gmail.com>
2024-04-18 12:12:51 +0700

    commit fix

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import githubAuth from "next-auth/providers/github"
 
-export const authOption = {
+const authOption = {
     providers: [
         githubAuth({
             clientId: process.env.GITHUB_CLIENT,
